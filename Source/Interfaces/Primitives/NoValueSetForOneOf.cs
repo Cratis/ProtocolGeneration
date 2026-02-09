527e09342e5305fb95cae7cc697906086ddc0523
Einar <einari@me.com>
2026-02-09 15:05:13 +0100

    Further scaffolding

---
+++ Source/Interfaces/Primitives/NoValueSetForOneOf.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Interfaces.Primitives;
+
+/// <summary>
+/// Exception that gets thrown when no value is set for a OneOf construct.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="NoValueSetForOneOf"/> class.
+/// </remarks>
+/// <param name="types">Types for the one of.</param>
+public class NoValueSetForOneOf(params Type[] types) : Exception($"No value set for OneOf<{string.Join(", ", types.Select(_ => _.Name))}>")
+{
+}
---
+++ Source/Server/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+var builder = WebApplication.CreateBuilder(args)
+    .AddCratisArc(
+        options =>
+        {
+            options.GeneratedApis.RoutePrefix = "api";
+            options.GeneratedApis.IncludeCommandNameInRoute = false;
+            options.GeneratedApis.SegmentsToSkipForRoute = 1;
+        });
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseWebSockets();
+app.MapControllers();
+app.UseCratisArc();
+app.MapFallbackToFile("/index.html");
+
+await app.RunAsync();
