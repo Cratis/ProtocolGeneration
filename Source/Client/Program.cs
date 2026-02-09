ce3bb485e3bb8ff52131a1e3271b3917d6b9f520
Einar <einari@me.com>
2026-02-09 14:28:31 +0100

    Making it compile

--- Source/Client/Program.cs
+++ Source/Client/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
